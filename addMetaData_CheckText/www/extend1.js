2a3a45ca9c03ee9fdf848d228ad5313dce2d3280
woordwaark <131461793+woordwaark@users.noreply.github.com>
2023-05-05 12:21:47 +0300

    Add files via upload

---
+++ addMetaData_CheckText/www/extend1.js
@@ -0,0 +1,57 @@
+/* Make textAreaInput auto-resizing the text box  */
+
+var observe;
+
+if (window.attachEvent) 
+{
+  observe = function (element, event, handler) 
+  {
+    element.attachEvent('on'+event, handler);
+  };
+}
+else 
+{
+  observe = function (element, event, handler) 
+  {
+    element.addEventListener(event, handler, false);
+  };
+}
+
+function init1 (inputID) 
+{
+  var text = document.getElementById(inputID);
+
+  text.style.minHeight = '32px';
+//text.style.minHeight = '52px';
+
+  function resize () 
+  {
+    text.style.height = 'auto';
+    text.style.height = text.scrollHeight+'px';
+  }
+  
+  /* 0-timeout to get the already changed text */
+  
+  function delayedResize () 
+  {
+    window.setTimeout(resize, 0);
+  }
+  
+  observe(text, 'click',   resize);
+  observe(text, 'change',  resize);
+  observe(text, 'cut',     delayedResize);
+  observe(text, 'paste',   delayedResize);
+  observe(text, 'drop',    delayedResize);
+  observe(text, 'keydown', delayedResize);
+
+  text.focus();
+  text.select();
+  resize();
+  
+  text.style.overflow = 'hidden';
+}
+
+init1('srcRedacteur');
+init1('srcTitel');
+init1('srcWebsite');
+init1('srcOpmerkingen');
